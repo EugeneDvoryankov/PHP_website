24e3a8f692d4133dc17c4c3c7ff7bed79f536b70
EugeneDvoryankov <kolontayev1@gmail.com>
2023-12-30 13:15:03 +0100

    Completed task 'Add text'

--- index.php
+++ index.php
@@ -3,23 +3,27 @@
     <head>
         <title>PHP Test</title>
         <style>
-            .background-image img {
+            .container img{
                 width: 100%;
                 height: 400px;
+                text-align: center;
             }
 
             .text-container{
-                position: relative;
-                top: -10px;
-                color:black;
+                color: white;
+                position: absolute;
+                top: 30%;
+                left: 50%;
+                transform: translate(-30%, -50%);
             }
 
         </style>
     </head>
-    
     <body>
-    <div class="background-image">
+
+    <div class="container">
             <img src="home_page_background.jpg">
+
             <div class="text-container">
                 <h1>Hi there!</h1>
                 <p>I am Eugene Dvoryankov</p>
