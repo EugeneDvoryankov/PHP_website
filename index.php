146353eb11d560247dc0486192b214b41f7eb2aa
EugeneDvoryankov <kolontayev1@gmail.com>
2023-12-30 12:52:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+        <style>
+            .background-image img {
+                width: 100%;
+                height: 400px;
+            }
+
+            .text-container{
+                position: relative;
+                top: -10px;
+                color:black;
+            }
+
+        </style>
+    </head>
+    
+    <body>
+    <div class="background-image">
+            <img src="home_page_background.jpg">
+            <div class="text-container">
+                <h1>Hi there!</h1>
+                <p>I am Eugene Dvoryankov</p>
+                <p>A Software Engineer</p>
+            </div>
+        </div>
+    </body>
+</html>
+    
